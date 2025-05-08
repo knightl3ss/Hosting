8d87290e67a8525dcd8f5ab55da1aafc1f12c631
knightl3ss <narismastem2019@gmail.com>
2025-05-08 16:26:12 +0800

    Add empty RomanNumerals class to satisfy the autoloader

---
+++ NewUpdate/app/Helpers/RomanNumerals.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Empty class to satisfy the autoloader
+ * This exists only because something in the remote environment is trying to load it
+ */
+class RomanNumerals
+{
+    // Empty class
+} 
