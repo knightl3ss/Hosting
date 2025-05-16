5204da22de825c59beda7a411f6d1f3d52a33235
KennyD <145532561+knightl3ss@users.noreply.github.com>
2025-05-16 13:33:16 +0800

    Update front_page.blade.php

--- resources/views/Pages/front_page.blade.php
+++ resources/views/Pages/front_page.blade.php
@@ -298,7 +298,7 @@
     <!-- Mini Footer -->
     <footer class="bg-dark text-light py-2 mt-auto w-100 shadow-sm mini-footer" style="position:fixed;bottom:0;left:0;z-index:1050;">
       <div class="container text-center small">
-        &copy; {{ date('Y') }} LGU Capstone. All rights reserved.
+        &copy; {{ date('Y') }} LGU Magallanes All rights reserved.
       </div>
     </footer>
 
